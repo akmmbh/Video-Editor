9b8bd1abf7e10a7f11d479f4f97bfa66bfe86c37
akmmbh <akmmbhmishra.mishra@gmail.com>
2025-04-16 23:41:46 +0000

    Added upload routes successfully

--- src/DB.js
+++ src/DB.js
@@ -2,9 +2,10 @@ const fs = require("node:fs");
 
 const usersPath = "./data/users";
 const sessionsPath = "./data/sessions";
-
+const videosPath = "./data/videos";
 class DB {
   constructor() {
+    this.video= JSON.parse(fs.readFileSync(videosPath,"utf8"));
     /*
      A sample object in this users array would look like:
      { id: 1, name: "Liam Brown", username: "liam23", password: "string" }
@@ -19,13 +20,15 @@ class DB {
   }
 
   update() {
+    this.video= JSON.parse(fs.readFileSync(videosPath,"utf8"));
     this.users = JSON.parse(fs.readFileSync(usersPath, "utf8"));
     this.sessions = JSON.parse(fs.readFileSync(sessionsPath, "utf8"));
   }
 
   save() {
-    fs.writeFileSync(usersPath, JSON.stringify(db.users));
-    fs.writeFileSync(sessionsPath, JSON.stringify(db.sessions));
+    fs.writeFileSync(videosPath,JSON.stringify(this.video));
+    fs.writeFileSync(usersPath, JSON.stringify(this.users));
+    fs.writeFileSync(sessionsPath, JSON.stringify(this.sessions));
   }
 }
 
--- src/controllers/video.js
+++ src/controllers/video.js
@@ -1,5 +1,18 @@
 const controller = require("./user");
 const path = require("node:path");
+const fs= require("node:fs/promises");
+const crypto=  require("node:crypto");
+const {pipeline}= require("node:stream/promises");
+const util = require("../../lib/util");
+
+const DB = require("../DB")
+
+
+
+
+
+
+
 const getVideos =(req,res,handleErr)=>{
     const name = req.params.get("name");
     if(name){
@@ -10,7 +23,7 @@ const getVideos =(req,res,handleErr)=>{
     }
     
 }
-const uploadVideo= (req,res,handleErr)=>{
+const uploadVideo= async (req,res,handleErr)=>{
     const specifiedFilename = req.headers.filename;
     if (!specifiedFilename) {
       return handleErr({ status: 400, message: "Filename is missing in headers." });
@@ -18,6 +31,62 @@ const uploadVideo= (req,res,handleErr)=>{
    
     const extension = path.extname(specifiedFilename).substring(1).toLowerCase();
     const name = path.parse(specifiedFilename).name;
+    const videoId= crypto.randomBytes(4).toString("hex");
+
+    try{
+   //we are grabbing the video id then make folder with same name
+    await fs.mkdir(`./storage/${videoId}`);
+    //original video path
+
+    const fullPath = `./storage/${videoId}/original.${extension}`;
+    //now we can create file at full path location
+    const file = await fs.open(fullPath,"w");
+    //now we want to write in file so we have to create a write stream
+    const fileStream = file.createWriteStream();
+    await pipeline(req,fileStream);
+
+ 
+
+    //updating our db
+    DB.update();
+    DB.video.unshift({
+        id:DB.video.length,
+        videoId,
+        name,
+        extension,
+        userId:req.userId,
+        extractedAudio:false,
+        resizes:{}
+    })
+    DB.save();
+
+    res.status(200).json({
+        status:"sucess",
+        message:"File was uploaded sucssuflly"
+    })
+
+
+
+
+
+
+
+
+
+
+
+    }catch(e){
+        //Delete the whole folder
+util.deleteFolder(`./storage/${videoId}`);
+if(e.code!=="ECONNRESET")return handleErr(e);
+
+    }
+
+ 
+
+
+
+
     console.log(specifiedFilename, extension, name);
   
 }
